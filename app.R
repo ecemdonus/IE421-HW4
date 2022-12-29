9ae8d2e7439ce828dd7bb1cdf8d453d9b7233150
ecemdonus <119009601+ecemdonus@users.noreply.github.com>
2022-12-29 15:14:41 +0300

    Create app.R

---
+++ app.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(datasets)
+library(ggplot2)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+ui <- fluidPage(
+  
+  titlePanel("Miles Per Gallon"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear")),
+      
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    mainPanel(
+      
+      h3(textOutput("caption")),
+      
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+ 
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgPlot <- renderPlot({
+    ggplot(mpgData,aes(x=mpg)) + 
+    geom_histogram(binwidth = 5,colour="white",fill="yellowgreen") + 
+      facet_wrap(input$variable, ncol = 1)
+    
+  })
+  
+}
+
+shinyApp(ui, server)
